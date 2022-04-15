51fd536955a2e68fb2a98f2513459b57e78d3768
honneyjung <guslwjd77@naver.com>
2022-04-15 11:01:29 +0900

    05 깃허브에 리액트 앱 업로드하기

--- src/App.js
+++ src/App.js
@@ -1,68 +1,37 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-// function Fruit(props){
-//   console.log(props);
-//   return(
-//     <h1>{props.fav}마시썽</h1>
-//   );
-// }
-
-function Fruit({name, picture, rating}){
-  return (
-  <div>
-    <h2>{name} 마시썽</h2>
-    <h4>{rating} / 5.0 </h4>
-    <img src={picture} alt={name} />
-  </div>
-  )
-}
-
-const fruitILike=[
-  {
-    id:1,
-    name: '바나나',
-    image:'http://qwerew.cafe24.com/images/banana.png',
-    rating:4,
-  },
-  {
-    id:2,
-    name: '오렌지',
-    image:'http://qwerew.cafe24.com/images/orange.png',
-    rating:3,
-  },
-  {
-    id:3,
-    name: '사과',
-    image:'http://qwerew.cafe24.com/images/apple.png',
-    rating:5,
-  },
-  {
-    id:4,
-    name: '메론',
-    image:'http://qwerew.cafe24.com/images/melon.jpg',
-    rating:3.5,
+class App extends React.Component{
+  constructor(props){
+    super(props)
+      console.log('constructor 실행완료');
   }
-]
 
-// function renderFruit(dish){
-//   return<Fruit key={dish.id} name={dish.name} picture={dish.image} />
-// }
-
-function App() {
-  return( 
-    <div>
-      {fruitILike.map((dish) => (
-      <Fruit key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
-      ))}
-    </div>
-  );
-}
-
-Fruit.propTypes={
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
+  state = { count: 0 }
+  add=()=>{
+    this.setState({count : this.state.count + 1})
+  }
+  minus=()=>{
+    this.setState({count : this.state.count - 1})
+  }
+  componentDidMount(){
+    console.log('componentDidMount 실행완료')
+  }
+  componentDidUpdate(){
+    console.log('componentDidUpdate 실행완료')
+  }
+  componentWillUnmount(){
+    console.log('componentWillUnmount 실행완료')
+  }
+  render(){
+    console.log('render 실행완료');
+    return (
+      <div>
+        <h1>번호는  {this.state.count}</h1>
+        <button onClick={this.add}>더하기</button>
+        <button onClick={this.minus}>빼기</button>
+      </div>
+    )
+  }
 }
 
 export default App;
